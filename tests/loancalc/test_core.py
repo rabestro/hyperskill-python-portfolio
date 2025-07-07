a9f3c5980d0a0117868d11e2208df1ed6f703f9a
Jegors Cemisovs <jcemisovs@evolution.com>
2025-07-07 22:51:14 +0300

    Refactor test cases in `test_core.py` to use explicit arguments instead of `LoanInput`.

--- tests/loancalc/test_core.py
+++ tests/loancalc/test_core.py
@@ -9,27 +9,21 @@ from loancalc.models import LoanInput
 
 def test_annuity_payment():
     """Tests the calculation of a fixed monthly annuity payment."""
-    res = calculate_annuity_payment(
-        LoanInput(principal=1000000, periods=12, interest=10)
-    )
+    res = calculate_annuity_payment(1 / 120, 12, 1000000)
     assert res.payment == 87916
     assert res.overpayment == 54992
 
 
 def test_annuity_principal():
     """Tests the calculation of the loan principal for an annuity loan."""
-    res = calculate_annuity_principal(
-        LoanInput(payment=8722, periods=120, interest=5.6)
-    )
+    res = calculate_annuity_principal(5.6 / 1200, 120, 8722)
     assert res.principal == 800018
     assert res.overpayment == 246622
 
 
 def test_annuity_periods():
     """Tests the calculation of the repayment period for an annuity loan."""
-    res = calculate_annuity_periods(
-        LoanInput(principal=500000, payment=23000, interest=7.8)
-    )
+    res = calculate_annuity_periods(7.8 / 1200, 500000, 23000)
     assert res.periods == 24
     assert "year" in res.description
     assert res.overpayment == 52000
